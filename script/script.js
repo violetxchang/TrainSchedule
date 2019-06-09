9b31d9bc0733ae1c4648a065ccbcb4770d5b39fa
violet chang <violetchang@violets-MacBook-Pro.local>
2019-06-09 18:34:56 -0500

    added firebase config

---
+++ script/script.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyB4jzm0tbmxFtXex4i8bE5-4GhSsb8RXG4",
+    authDomain: "users-51553.firebaseapp.com",
+    databaseURL: "https://users-51553.firebaseio.com",
+    projectId: "users-51553",
+    storageBucket: "users-51553.appspot.com",
+    messagingSenderId: "80362863635",
+    appId: "1:80362863635:web:881e543d3543dd5f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
